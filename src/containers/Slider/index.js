d4710f5ac2c54f88d332f908795510b33e016a62
ju2dev <j.piergiovanni@icloud.com>
2024-12-10 17:50:05 +0100

    premier commit

---
+++ src/containers/Slider/index.js
@@ -0,0 +1,68 @@
+import { useEffect, useState } from "react";
+import { useData } from "../../contexts/DataContext";
+import { getMonth } from "../../helpers/Date";
+
+import "./style.scss";
+
+const Slider = () => {
+  const { data } = useData();
+  const [index, setIndex] = useState(0);
+
+  // Cela évite une erreur si `data` ou `data.focus` est undefined.
+  const byDateDesc = data?.focus
+    ? [...data.focus].sort((evtA, evtB) =>
+        new Date(evtA.date) > new Date(evtB.date) ? -1 : 1
+      )
+    : []; // Si `data.focus` est vide ou non défini, on retourne un tableau vide.
+
+  // Ajout de `clearTimeout` pour éviter l'accumulation de timeouts.
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setIndex(index < byDateDesc.length - 1 ? index + 1 : 0);
+    }, 5000);
+
+    // Nettoyage du timeout précédent à chaque rendu.
+    return () => clearTimeout(timer);
+  }, [index, byDateDesc.length]); // Ajout des dépendances `index` et `byDateDesc.length`.
+
+  return (
+    <div className="SlideCardList">
+      {byDateDesc.map((event, idx) => (
+        // On utilise `event.id` si disponible, ou l'index `idx` comme dernier recours.
+        <div key={event.id || idx}>
+          <div
+            className={`SlideCard SlideCard--${
+              index === idx ? "display" : "hide"
+            }`}
+          >
+            <img src={event.cover} alt="forum" />
+            <div className="SlideCard__descriptionContainer">
+              <div className="SlideCard__description">
+                <h3>{event.title}</h3>
+                <p>{event.description}</p>
+                <div>{getMonth(new Date(event.date))}</div>
+              </div>
+            </div>
+          </div>
+          {index === idx && (
+            <div className="SlideCard__paginationContainer">
+              <div className="SlideCard__pagination">
+                {byDateDesc.map((eventslide, radioIdx) => (
+                  <input
+                    key={`${eventslide.id || radioIdx}`} // On combine `id` ou `radioIdx` pour assurer l'unicité.
+                    type="radio"
+                    name="radio-button"
+                    checked={idx === radioIdx}
+                    readOnly
+                  />
+                ))}
+              </div>
+            </div>
+          )}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Slider;
